5c4d584e676f2193e8e0e8e0fd617b9e21eff012
Marco Ferreira <marcoferreiraperson@gmail.com>
2024-10-10 20:12:51 -0400

    initial commit

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type tasks struct  {
+
+}
+
+func main() {
+	// create a todo cli application
+
+	var done bool = false
+	for _: !done: _{
+		
+		fmt.Scan("Please select an option: ")
+	}
+	
+
+}
